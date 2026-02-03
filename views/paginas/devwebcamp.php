3342210245b89ba372750344817c2170d747e3e8
Niariasve <niarias@espol.edu.ec>
2026-02-03 18:02:35 -0500

    Pagina devwebcamp

---
+++ views/paginas/devwebcamp.php
@@ -0,0 +1,29 @@
+<main class="devwebcamp">
+  <h2 class="devwebcamp__heading"><?= $titulo ?></h2>
+  <p class="devwebcamp__descripcion">Conoce la conferencia más importante de latinoamérica</p>
+
+  <div class="devwebcamp__grid">
+    <div class="devwebcamp__imagen">
+      <picture>
+        <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+        <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+        <img src="build/img/sobre_devwebcamp.jpg" alt="imagen devwebcamp" loading="lazy">
+      </picture>
+    </div>
+
+    <div class="devwebcamp__contenido">
+      <p class="devwebcamp__texto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, 
+        voluptatem deleniti sequi autem cum dolor unde, enim consectetur incidunt vitae 
+        debitis iste quod omnis et exercitationem ipsum eaque a velit!
+      </p>
+
+      <p class="devwebcamp__texto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, 
+        voluptatem deleniti sequi autem cum dolor unde, enim consectetur incidunt vitae 
+        debitis iste quod omnis et exercitationem ipsum eaque a velit!
+      </p>
+    </div>
+  </div>
+
+</main>
