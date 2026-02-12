b889b4c7eea41a909aa51d1ce3e6df08293c73f7
Niariasve <niarias@espol.edu.ec>
2026-02-11 20:26:33 -0500

    💄 Estilos para speakers en pagina inicio

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -27,38 +27,42 @@ include_once __DIR__ . '/conferencias.php';
   <h2 class="speakers__heading">Speakers</h2>
   <p class="speakers__descripcion">Conoce a nuestros expertos de devwebcamp</p>
 
-  <?php foreach ($ponentes as $ponente): ?>
-    <div class="speaker">
-      <picture>
-        <source srcset="<?= $_ENV['HOST'] . "/img/speakers/" . $ponente->imagen . ".webp" ?>" type="image/webp">
-        <source srcset="<?= $_ENV['HOST'] . "/img/speakers/" . $ponente->imagen . ".png" ?>" type="image/png">
-        <img class="speaker__imagen" loading="lazy" width="200" height="300" src="<?= $_ENV['HOST'] . "/img/speakers/" . $ponente->imagen . ".png" ?>" alt="imagen ponente">
-      </picture>
+  <div class="speakers__grid">
 
-      <div class="speaker__informacion">
-        <h4 class="speaker__nombre"><?= $ponente->nombre . ' ' . $ponente->apellido ?></h4>
 
-        <p class="speaker__ubicacion"><?= $ponente->ciudad . ', ' . $ponente->pais ?></p>
+    <?php foreach ($ponentes as $ponente): ?>
+      <div class="speaker">
+        <picture>
+          <source srcset="<?= $_ENV['HOST'] . "/img/speakers/" . $ponente->imagen . ".webp" ?>" type="image/webp">
+          <source srcset="<?= $_ENV['HOST'] . "/img/speakers/" . $ponente->imagen . ".png" ?>" type="image/png">
+          <img class="speaker__imagen" loading="lazy" width="200" height="300" src="<?= $_ENV['HOST'] . "/img/speakers/" . $ponente->imagen . ".png" ?>" alt="imagen ponente">
+        </picture>
 
-        <nav class="speaker__sociales">
-          <?php $redes = json_decode($ponente->redes) ?>
-          <?php foreach ($redes as $red => $str): ?>
-            <?php if (!empty($str)): ?>
-              <a class="speaker__enlace" rel="noopener noreferrer" target="_blank" href="<?= $str ?>">
-                <span class="speaker__ocultar"><?= strtoupper($red) ?></span>
-              </a>
-            <?php endif ?>
-          <?php endforeach ?>
-        </nav>
+        <div class="speaker__informacion">
+          <h4 class="speaker__nombre"><?= $ponente->nombre . ' ' . $ponente->apellido ?></h4>
 
-        <ul class="speaker__listado-skills">
-          <?php
-          $tags = explode(',', $ponente->tags);
-          foreach ($tags as $tag): ?>
-            <li class="speaker_skill"><?= $tag ?></li>
-          <?php endforeach ?>
-        </ul>
+          <p class="speaker__ubicacion"><?= $ponente->ciudad . ', ' . $ponente->pais ?></p>
+
+          <nav class="speaker-sociales">
+            <?php $redes = json_decode($ponente->redes) ?>
+            <?php foreach ($redes as $red => $str): ?>
+              <?php if (!empty($str)): ?>
+                <a class="speaker-sociales__enlace" rel="noopener noreferrer" target="_blank" href="<?= $str ?>">
+                  <span class="speaker-sociales__ocultar"><?= $red ?></span>
+                </a>
+              <?php endif ?>
+            <?php endforeach ?>
+          </nav>
+
+          <ul class="speaker__listado-skills">
+            <?php
+            $tags = explode(',', $ponente->tags);
+            foreach ($tags as $tag): ?>
+              <li class="speaker_skill"><?= $tag ?></li>
+            <?php endforeach ?>
+          </ul>
+        </div>
       </div>
-    </div>
-  <?php endforeach ?>
+    <?php endforeach ?>
+  </div>
 </section>
